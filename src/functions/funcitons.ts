006d6536238e16ad9a6fdbc6b4d2c84a5dc2a43b
Marcello Farias <marcellorocha632@gmail.com>
2024-07-18 10:53:33 -0300

    Aplicando leitura biométrica ao invés de confirmação de senha de acesso

--- src/functions/funcitons.ts
+++ src/functions/funcitons.ts
@@ -1,5 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { SheetManager } from "react-native-actions-sheet";
+import * as LocalAuthentication from "expo-local-authentication";
+import Toast from "react-native-toast-message";
 
 export function renderLimitChars(text: string) {
   if (text.length < 20) {
@@ -14,6 +16,17 @@ export async function openConfirmationPasswordSheet(onConfirm: () => void) {
 
   if (configs !== null) {
     const configsObj = JSON.parse(configs);
+
+    if (configsObj?.allowBiometry) {
+      const authenticated = await LocalAuthentication.authenticateAsync({
+        promptMessage: "Utilize a biometria para realizar esta ação",
+      });
+
+      if (authenticated.success) {
+        onConfirm();
+        return;
+      }
+    }
 
     if (configsObj?.accessPassword !== "") {
       SheetManager.show("ConfirmPassword-sheet", {
